0905f88efbed272fc6a9e7da3df8888e5953dc63
MobuchiElly <ugwueleazer@gmail.com>
2024-07-06 13:54:17 +0100

    setting up project

---
+++ pages/home.js
@@ -0,0 +1,37 @@
+Stage 2 Task: Design and frontend task
+Design & Build a Mini Online Store!
+Task Description:
+Get ready to collaborate with a front-end developer to build a simple and user-friendly online store, focusing on a fast and streamlined shopping experience. This will be a single-seller platform showcasing products and checkout experience.
+How to pair up:
+Designers and developers will be added to a private channel to bid.
+Designers in the channel will introduce yourselves. In a single post, a designer will pitch themselves to developers while sharing images of their best designs in the thread of their post. (posting of links are not allowed, also YOU ARE NOT ALLOWED TO POST TWICE to avoid confusion).
+Frontend developers will choose the designer they'd like to collaborate with and share a link to their past live projects in the thread of the designers they would like to work with (A FRONTEND DEVELOPER IS ONLY ALLOWED TO WORK WITH ONE DESIGNER)
+Once both agree to work together, the designer is to fill this form with their slack ID and that of the developer to record the partnership.
+Pairing rules and regulations:
+You have a timeframe of 3-4hours to be paired UP after adding you to the bidding channel.
+Failure to be paired after that timeframe means you're not interested pairing up with the designers in the channel, and for that, you'll be added to another pool of designers to bid for.
+The earlier you pair up, the earlier your designer have time to work and you have time to implement.
+What to Design:
+(Here’s a sample of a seller’s shop. This should serve as a guide and NOT to be copied and pasted - designers BEWARE!)
+Seller's Product Listing Page (web, mobile responsive, mobile app view)
+Cart Page (web, mobile responsive, mobile app view)
+Checkout experience(web, mobile responsive, mobile app view)
+What should be implemented:
+Seller's Product Listing Page (web, mobile responsive)
+Cart Page (web, mobile responsive)
+Checkout experience(web, mobile responsive)
+Pair Deliverables:
+A live link to your fully functional online store that works across all browsers (YOU WON’T BE SUBMITTING YOUR FIGMA FILE)
+How You'll Be Graded:
+Easy and enjoyable user experience of the implemented live link
+Looks good on all devices (responsive design)
+Clean and easy-to-use design
+Consistent brand style throughout
+Simple and fast-loading time on the live link
+Deadline & Notes:
+Monday, 8th - 11:59 PM GMT+1 (strict deadline) - live link deadline
+Each pair will be graded based on the live link to their fully functional online store that WORKS, fully responsive and looks good across all browsers and devices (Figma link WILL NOT be submitted and used for grading)
+Frontend developers can only work with one designer.
+If you're in BOTH design and frontend tracks, you can design and build your own store (inform both track mentors of the solo paired work to take record).
+Bid for a partner early to give yourselves more time to work!
+Submission Link: Submit here
