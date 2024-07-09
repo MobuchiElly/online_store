82038892361e97f792eaeeec000ff113526bf2e4
MobuchiElly <ugwueleazer@gmail.com>
2024-07-09 22:00:04 +0100

    ui update

--- components/FComponents/CartProductCard.js
+++ components/FComponents/CartProductCard.js
@@ -9,9 +9,9 @@ const CheckoutProductCard = ({product}) => {
   }
 
   return (
-    <div className="p-2 flex">
-      <div className="rounded-lg overflow-hidden md:flex w-full md:w-[80%] lg:w-[90%]">
-        <div className="relative w-full h-64 px-2 py-1 md:px-10 lg:px-20">
+    <div className="md:px-2 py-2 flex">
+      <div className="rounded-lg overflow-hidden flex w-full md:w-[80%] lg:w-[90%]">
+        <div className="relative w-2/5 h-64 pl-2 py-1 md:px-10 lg:px-20">
           <div className="h-64 bg-slate-100">
               <Image
                 src={product.url}
@@ -23,13 +23,16 @@ const CheckoutProductCard = ({product}) => {
           </div>
         </div>
 
-        <div className="p-4 border [w-10%] lg:flex lg:flex-col lg:justify-center ">
+        <div className="pt-10 md:pt-5 px-1 md:px-4 md:p-4 border w-2/4 lg:flex lg:flex-col lg:justify-center ">
           <h3 className="text-sm font-extralight mb-2">Designed by Feramo</h3>
           <p className="mb-2 font-bold">{product.name}</p>
 
           <div className="flex justify-between items-center text-sm">
             <span className="text-lg font-bold mr-4">Variations: </span>
-            <button className="border border-black w-12 px-1 ml-1">XXL</button>
+            <button className="border border-black w-12 mx-1 py-1">S</button>
+            <button className="border border-black w-12 mx-1 py-1">M</button>
+            <button className="border border-black w-12 mx-1 py-1">L</button>
+            <button className="border border-black w-12 px-1 ml-1 py-1">XXL</button>
           </div>
           <div className="items-center my-2">
             <span className="text-lg font-bold">Price: {product.price}</span>
