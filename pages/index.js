03906ed30be789118ef8a2a6c4d97306ba8a06b6
MobuchiElly <ugwueleazer@gmail.com>
2024-07-09 20:10:28 +0100

    implemented meta data for seo

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 const index = () => {
   return (
     <div style={{height:"calc(100vh - 20vh)"}}>
-      index
+      <Head>
+        <title>Lapis Online Shopping Experience</title>
+        <meta name="description" content="Trending and Exclusive Mens wears just for you" />
+        <meta name="keywords" content="mens wear, trending, exclusive" />
+      </Head>
     </div>
   )
 }
