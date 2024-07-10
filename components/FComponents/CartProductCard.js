36e446f64cd74be38f439b9ec60e8b9a61e9c722
MobuchiElly <ugwueleazer@gmail.com>
2024-07-10 17:54:40 +0100

    ui update

--- components/FComponents/CartProductCard.js
+++ components/FComponents/CartProductCard.js
@@ -26,26 +26,28 @@ const CheckoutProductCard = ({product}) => {
           </div>
         </div>
 
-        <div className="pt-16 md:pt-5 px-1 md:px-4 md:p-4 w-3/5 lg:flex lg:flex-col lg:justify-center">
+        <div className="pt-10 md:pt-5 px-1 md:px-4 md:p-4 w-3/5 lg:flex lg:flex-col lg:justify-center space-y-2 lg:space-y-3">
           <h3 className="text-sm font-extralight mb-2">Designed by Feramo</h3>
           <p className="mb-2 font-bold">{product.name}</p>
 
-          <div className="flex  items-center text-sm">
-            <span className="text-lg font-bold md:mr-4 ">Variation:</span>
+          <div className="lg:flex items-center text-sm">
+            <p className="text-base font-bold md:mr-4 ">Variation:</p>
             <button className="border border-black mx-1 p-1 md:px-3  md:mx-2">S</button>
             <button className="border border-black  mx-1 p-1">M</button>
             <button className="mx-1 px-3 py-2 md:px-3 md:mx-2 bg-mainBg text-white">L</button>
+            <button className="border border-black p-1 ml-1 py-1 md:px-3  md:mx-2">XL</button>
             <button className="border border-black p-1 ml-1 py-1 md:px-3  md:mx-2">XXL</button>
           </div>
-          <div className="items-center my-2">
-            <span className="text-lg font-bold">Price: {product.price}</span>
+          <div className="flex items-center my-2 lg:justify-end">
+            <p className="text-2xl font-bold lg:text-right pr-14 lg:pr-6 lg:flex">
+              <span className="hidden lg:block mr-2 font-semibold text-lg pt-1">Price: </span>{product.price}</p>
           </div>
           <div className="flex justify-between items-center font-semibold">
             <button className="hidden lg:block py-2 px-3 rounded-2xl border border-gray-700 lg:border-gray-500 text-layoutMainBg lg:text-black lg:bg-white font-medium mr-8 text-sm hover:scale-105">Delete</button>
             <div className="flex">
-              <button className="border border-black px-3 py-1 mx-2 hover:scale-105">-</button>
+              <button className="border border-black px-3 py-1 mx-2 hover:scale-105 bg-mainBg">-</button>
               <span className="mx-4 text-lg font-bold pt-1">1</span>
-              <button className="border border-black hover:scale-105 px-3 py-1 mx-2">+</button>
+              <button className="border border-black hover:scale-105 px-3 py-1 mx-2 bg-mainBg">+</button>
             </div>
           </div>
         </div>
