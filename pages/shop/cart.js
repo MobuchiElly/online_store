d55f75b7bf8db5300fc2b229463ed0aae7a704cf
MobuchiElly <ugwueleazer@gmail.com>
2024-07-10 20:35:33 +0100

    ui update

--- pages/shop/cart.js
+++ pages/shop/cart.js
@@ -17,30 +17,27 @@ const Cart = () => {
       <div className=" px-2 md:px-5 lg:px-12 py-4">
         <div>
           <h1 className="text-2xl font-bold lg:text-center mb-5 pt-2 capitalize">Cart Summary</h1>
-        
-          <div className="lg:flex">
-            <main className="w-full">
-              <section className="border-b border-slate-400 font-semibold lg:px-6">
-                <p className="inline-flex font-semibold text-lg">
-                  <span className="bg-mainBg rounded-full flex justify-center items-center h-7 w-8 text-black mr-2 text-lg font-bold">3</span> Item(s) Selected
-                </p>
-                <p className="pl-1 p-1">Subtotal: <span className="font-bold text-xl">$150.00</span></p>
-              </section>
-              <section className="border-b border-slate-400">
-              <CartProductCard product={products[0]}/>
-              </section>
-              <section className="border-b border-slate-400">
-              <CartProductCard product={products[1]}/>
-              </section>
-              <section className="border-b border-slate-400">
-              <CartProductCard product={products[2]}/>
-              </section>
-              <section className="h-16 mb-3 md:mb-6 border-b border-slate-400 mt-2">
-                <div className="flex justify-center items-center h-full"><Link href="/shop/checkout" className="bg-mainBg rounded-xl w-[80%] textwhite py-2 text-black font-bold text-center hover:bg-[#C05F3C] lg:ml-24 lg:pl-4">Proceed to checkout $150</Link></div>
-              </section>
-            </main>
-          </div>
-        
+          <main className="w-full">
+            <section className="border-b border-slate-400 font-semibold lg:px-6">
+              <p className="inline-flex font-semibold text-lg">
+                <span className="bg-mainBg rounded-full flex justify-center items-center h-7 w-8 text-black mr-2 text-lg font-bold">3</span> Item(s) Selected
+              </p>
+              <p className="pl-1 p-1">Subtotal: <span className="font-bold text-xl">$150.00</span></p>
+            </section>
+            <section className="border-b border-slate-400">
+            <CartProductCard product={products[0]}/>
+            </section>
+            <section className="border-b border-slate-400">
+            <CartProductCard product={products[1]}/>
+            </section>
+            <section className="border-b border-slate-400">
+            <CartProductCard product={products[2]}/>
+            </section>
+            <section className="h-16 mb-3 md:mb-6 border-b border-slate-400 mt-2">
+              <div className="flex justify-center items-center h-full"><Link href="/shop/checkout" className="bg-mainBg rounded-xl w-[80%] textwhite py-2 text-black font-bold text-center hover:bg-[#C05F3C] lg:ml-24 lg:pl-4">Proceed to checkout $150</Link></div>
+            </section>
+          </main>
+          
           <section className="h-20 pt-4 pb-1 flex items-center">
             <p className="font-bold text-xl lg:px-6">You may also like</p></section>
           <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-0 pb-4">
